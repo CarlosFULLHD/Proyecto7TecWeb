23773e4b577b0979f7b2defba4169be9dc49a658
Natalia <bgutisol2019@gmail.com>
2023-04-12 13:42:57 -0400

    contactos

---
+++ js/contactos.js
@@ -0,0 +1,16 @@
+function presionBotonFacebook()
+{
+  alert('Redireccionamiento red social Facebook');
+}
+function presionBotonInstagram()
+{
+  alert('Redireccionamiento red social Instagram');
+}
+function presionBotonWhatsapp()
+{
+  alert('Redireccionamiento red social Whatsapp');
+}
+function presionBotonGitHub()
+{
+    location.replace('https://github.com/jp584/Proyecto7TecWeb');
+}
